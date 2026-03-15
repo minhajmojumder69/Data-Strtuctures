eb50f4629858850c3c354b983944cf206ebef7e7
minhajmojumder69 <minhajmojumder735@gmail.com>
2026-03-16 01:56:54 +0600

    DS Final

---
+++ Phitron (DS)/Module 18/Level_Nodes.cpp
@@ -0,0 +1,104 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int val;
+    Node *left;
+    Node *right;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+Node *input_tree()
+{
+    int val;
+    cin >> val;
+    Node *root;
+    if (val == -1)
+        root = NULL;
+    else
+        root = new Node(val);
+    queue<Node *> q;
+    if (root)
+        q.push(root);
+    while (!q.empty())
+    {
+        Node *p = q.front();
+        q.pop();
+
+        int l, r;
+        cin >> l >> r;
+        Node *myleft, *myright;
+        if (l == -1)
+        {
+            myleft = NULL;
+        }
+        else
+        {
+            myleft = new Node(l);
+        }
+        if (r == -1)
+        {
+            myright = NULL;
+        }
+        else
+        {
+            myright = new Node(r);
+        }
+        p->left = myleft;
+        p->right = myright;
+
+        if (p->left)
+        {
+            q.push(p->left);
+        }
+        if (p->right)
+        {
+            q.push(p->right);
+        }
+    }
+    return root;
+}
+void level_order(Node *root,int x)
+{
+    queue<pair<Node *,int>> q;
+    q.push({root,0});
+    int level;
+    while (!q.empty())
+    {
+        pair<Node *,int> parent = q.front();
+        q.pop();
+        Node* node = parent.first;
+        level = parent.second;
+
+        if (level == x)
+        {
+            cout << node->val << " ";
+        }
+        if (node->left)
+            q.push({node->left,level+1});
+        if (node->right)
+            q.push({node->right,level+1});
+    }
+    if (x > level)
+    {
+        cout << "Invalid";
+    }
+    
+}
+int main()
+{
+    Node *root = input_tree();
+    
+    int x;
+    cin >> x;
+
+    level_order(root,x);
+
+    return 0;
+}
